8a9188e68297d53718f844573d3c594bfe5c3061
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-16 20:51:50 +0000

    implemented addToDo case to reducer

--- frontend/src/reducers/todoReducer.js
+++ frontend/src/reducers/todoReducer.js
@@ -1,9 +1,11 @@
-import { FETCH_TODOS } from "../actions/types/todoTypes";
+import { FETCH_TODOS, ADD_TODO } from "../actions/types/todoTypes";
 
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_TODOS:
       return { ...state, todos: action.payload };
+    case ADD_TODO:
+      return { ...state, todos: [...state.todos, action.payload] }; 
     default:
       return state;
   }
