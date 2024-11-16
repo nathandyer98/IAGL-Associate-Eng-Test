9a515b4d3cf964fc459e6e07cfc59a04d15084da
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-16 21:14:33 +0000

    added error catching to action creators

--- frontend/src/actions/todoActions.js
+++ frontend/src/actions/todoActions.js
@@ -5,30 +5,35 @@ export function fetchTodos() {
   return function(dispatch) {
     return axios.get("http://localhost:9091/api/todo").then(({ data }) => {
       dispatch(setTodos(data.todos));
-    });
+    })
+    .catch((error) => {
+      console.error("Error fetching todo:", error);
+    })
   };
-}
+};
+
 
 export function addTodo(todo) {
   return function(dispatch) {
-    return axios.post(
-      "http://localhost:9091/api/todo", todo).then(({ data }) => {
+    return axios.post("http://localhost:9091/api/todo", todo).then(({ data }) => {
         dispatch(appendTodo(data));
-      }); 
+      })
+      .catch((error) => {
+        console.error("Error adding todo:", error);
+      }) 
   };  
-
-  }
+};
 
 function setTodos(data) {
   return {
     type: FETCH_TODOS,
     payload: data
   };
-}
+};
 
 function appendTodo(todo) {
   return {
     type: ADD_TODO,
     payload: todo
   };
-}
+};
